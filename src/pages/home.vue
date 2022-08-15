8e8e99a760024d8b30eb8c7e236e9ae9c2c9831c
Incorrect111 <alexeev.andrey1987@gmail.com>
2022-08-15 23:12:46 +0300

    moved form to component

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>hello home</h1>
+  <div class="md-body">
+    <Form @onSubmit="handleSubmit" />
+  </div>
 </template>
+
+<script>
+import Form from '@/components/Notes/Form.vue'
+
+export default {
+  components: { Form },
+  methods: {
+    handleSubmit(note) {
+      console.log(note)
+    }
+  }
+}
+</script>

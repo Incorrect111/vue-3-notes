c8b5d8a53ea14bb3ded3467a6b23ccda975e4d14
Incorrect111 <alexeev.andrey1987@gmail.com>
2022-08-16 00:50:28 +0300

    add render notes

---
+++ src/components/Notes/List.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="notes-list">
+    <div class="note-item" v-for="(note, index) in notes" :key="index">
+      <div class="note-header">
+          {{ note }}
+          <p style="cursor: pointer;" @click="$emit('onRemove', index)">&#x2715;</p>
+      </div>
+      <!-- <div class="note-footer"></div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notes: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.notes-list {
+  padding: 40px 0;
+}
+.note-item {
+  width: 100%;
+  padding: 18px 20px;
+  margin-bottom: 20px;
+  border-radius: 14px;
+  background-color: #ffffff;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+  box-shadow: 0 30px 30px rgba(0, 0, 0, 0.02);
+  &:hover {
+    box-shadow: 0 30px 30px rgba(0, 0, 0, 0.04);
+    transform: translate(0, -6px);
+    transition-delay: 0s !important;
+  }
+}
+.note-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.note-footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="md-body">
-    <Form @onSubmit="handleSubmit" />
-  </div>
+  <Form @onSubmit="handleSubmit" />
+  <List @onRemove="handleRemove" :notes="notes" />
 </template>
 
 <script>
 import Form from '@/components/Notes/Form.vue'
+import List from '@/components/Notes/List.vue'
 
 export default {
-  components: { Form },
+  components: { Form, List },
+  data() {
+    return {
+       notes: ['task1','task2','task3'],
+    }
+  },
   methods: {
     handleSubmit(note) {
-      console.log(note)
+      this.notes.push(note)
+    },
+    handleRemove(index) {
+      this.notes.splice(index, 1);
     }
   }
 }

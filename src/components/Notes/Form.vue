332c8e479a6fb5a40c679a52ea9484cc81fc1355
Incorrect111 <alexeev.andrey1987@gmail.com>
2022-08-15 23:26:20 +0300

    moved form to component

---
+++ src/components/Notes/Form.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="note-form__wrapper">
+  <form class="note-form" @submit.prevent="onSubmit">
+    <textarea type="text" required v-model="inputValue" placeholder="Type your note" />
+    <button class="btn btnPrimary" type="submit">Add note</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('onSubmit', this.inputValue)
+      console.log(this.inputValue)
+      this.inputValue = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+.note-form__wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.note-form{
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  width: 100%;
+}
+
+
+</style>

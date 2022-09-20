784bc166ded5584a04b675e7eeaea87f435c2634
Incorrect111 <alexeev.andrey1987@gmail.com>
2022-09-20 16:51:08 +0300

    delete store from main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/scss/main.scss'
 import router from './routes';
-import { store } from './store'
+
 
 
 const app = createApp(App);
 app.use(router);
-app.use(store);
 app.mount('#app');
